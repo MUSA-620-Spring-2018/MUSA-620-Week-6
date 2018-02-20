4073709fc052cbd05c27634d933883e4cbd36b36
Max Galka <galkamaxd@gmail.com>
2018-02-20 15:53:41 -0500

    Update web-inspector-data.R

--- web-inspector-data.R
+++ web-inspector-data.R
@@ -8,7 +8,7 @@ library(viridis)
 myTheme <- function() {
   theme_void() + 
     theme(
-      text = element_text(size = 8),
+      text = element_text(size = 9),
       plot.title = element_text(size = 14, color = "#111111", hjust = 0, vjust = 0, face = "bold"), 
       plot.subtitle = element_text(size = 12, color = "#333333", hjust = 0, vjust = 0),
       axis.ticks = element_blank(),
@@ -19,8 +19,21 @@ myTheme <- function() {
       legend.key.height = unit(0.4, "cm"), legend.key.width = unit(1, "cm"),
       legend.title = element_text(size = 9, color = "#111111", hjust = 0, vjust = 0),
       legend.text = element_text(size = 8, color = "#333333", hjust = 0, vjust = 0)
-    ) 
+    )
 }
+
+
+
+# ****** WEB INSPECTOR: ELEMENTS TAB ******
+
+# Download a hidden video file
+# https://i.imgur.com/9M4Rsf0.gifv
+
+download.file("https://i.imgur.com/9M4Rsf0.mp4","nyc-in-love.mp4")
+
+
+
+# ****** WEB INSPECTOR: ELEMENTS TAB ******
 
 
 # *** FiveThirtyEight: Congressional Districts ***
@@ -28,7 +41,7 @@ myTheme <- function() {
 # https://projects.fivethirtyeight.com/redistricting-maps/
 
 # This file is in topojson format, a compressed variant of geojson
-topojson <- readOGR("https://projects.fivethirtyeight.com/redistricting-maps/US-current.topo.json")
+topojson <- readOGR("https://null:null@projects.fivethirtyeight.com/redistricting-maps/US-current.topo.json")
 plot(topojson)
 
 # The coodinate system is screen coordinates -- need to flip the y axis
@@ -36,9 +49,10 @@ topojson <- elide(topojson, reflect=c(FALSE, TRUE))
 
 topojson <- st_as_sf(topojson)
 ggplot() +
-  geom_sf(data = topojson2, fill="#bbbbbb", color="black") +
+  geom_sf(data = topojson, fill="#bbbbbb", color="black") +
   myTheme()
 
+head(topojson)
 
 
 # *** WSJ: California Drought ***
@@ -60,7 +74,7 @@ caDrought <- readOGR("http://graphics.wsj.com/californias-long-challenge-with-dr
 caDrought20111227 <- filter(caDrought, filename=="USDM_20111227")
 ggplot() +
   geom_sf(data=caBorder,color="white") +
-  geom_sf(data = caDrought20160105, aes(fill = as.factor(dm)), color="black") +
+  geom_sf(data = caDrought20111227, aes(fill = as.factor(dm)), color="black") +
   scale_fill_viridis(discrete = TRUE, direction = -1, option="magma", name="Drought Intensity ") +
   myTheme()
 
@@ -89,13 +103,48 @@ ggplot() +
 
 
 
-
 # Washington Post: Where America’s sunniest and least-sunny places are
 #
 # https://www.washingtonpost.com/news/wonk/wp/2015/07/13/map-where-americas-sunniest-and-least-sunny-places-are/
-# See if you can find the data and recreate their sunlight map
 
-# Note: topojson files can have multiple layers
-#   Use ogrListLayers(URL) to view the layer names
-#   Use readOGR(URL,LAYER_NAME) to load a specific layer
+# In the network tab, look for these two files and copy out their URLs:
+# sunlight.csv
+# us.json
+
+# topojson files can have multiple layers
+counties <- readOGR("https://www.washingtonpost.com/graphics/business/sunlight/data/us.json")
+ogrListLayers("https://www.washingtonpost.com/graphics/business/sunlight/data/us.json")
+
+# you can specify the layer ("counties") when you load the file
+counties <- readOGR("https://www.washingtonpost.com/graphics/business/sunlight/data/us.json","counties") %>%
+  st_as_sf()
+
+# read in the csv
+sunlight <- read.csv("https://www.washingtonpost.com/graphics/business/sunlight/data/sunlight.csv")
+
+# join the csv data to the map
+sunlight$code <- as.character(sunlight$code) 
+counties$id <- as.character(counties$id)
+countysunlight <- left_join(counties,sunlight,by=c("id"="code"))
+
+# Remove HI, AK, PR for display purposes
+countysunlight$county <- as.character(countysunlight$county)
+countysunlight <- mutate(countysunlight,state = substr(county,nchar(county)-1,nchar(county)))
+countysunlight <- filter(countysunlight, state != "HI") %>%
+  filter(state != "AK") %>%
+  filter(state != "PR")
+
+# set the CRS 
+st_crs(countysunlight) = 4326
+
+ggplot() +
+  geom_sf(data = countysunlight, aes(fill=avg), color=alpha("black",0.3), size=0.1) +
+  scale_fill_viridis(discrete=FALSE , direction = 1, option="magma", name="Daily sunlight  ") +
+  coord_sf(crs = st_crs(102003)) +
+  labs(
+    title = 'Average Daily Sunlight by County',
+    subtitle = "Measured as solar radiation per square meter",
+    caption = "Source: Washington Post"
+  ) +
+  myTheme()
 
